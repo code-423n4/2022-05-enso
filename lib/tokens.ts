e14092a922593c78e04743577d433e0a8f7462b4
Peter Phillips <github@pmphillips.ca>
2022-05-03 15:40:14 -0700

    Fix tabbing

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -3,13 +3,13 @@ import { Contract } from 'ethers'
 import { ITEM_CATEGORY, ESTIMATOR_CATEGORY } from "./constants"
 
 export class Tokens {
-  // Basic
+	// Basic
 	weth: string
 	wbtc: string
 	dai: string
-  usdc: string
-  usdt: string
-  usdp: string
+	usdc: string
+	usdt: string
+	usdp: string
 	tusd: string
 	usdn: string
 	eurs: string
@@ -39,11 +39,11 @@ export class Tokens {
 	cDAI: string
 	cUSDC: string
 	cUSDT: string
-  // Curve
-  crv3: string
+	// Curve
+	crv3: string
 	crvTriCrypto: string
 	crvTriCrypto2: string
-  crvUSDP: string
+	crvUSDP: string
 	crvSUSD: string
 	crvAAVE: string
 	crvSAAVE: string
@@ -55,17 +55,17 @@ export class Tokens {
 	crvREN: string
 	crvEURS: string
 	// Curve Gauge
-  crvUSDPGauge: string
+	crvUSDPGauge: string
 	crvAAVEGauge: string
 	crvSAAVEGauge: string
 	crvLINKGauge: string
 	crvCOMPGauge: string
 	crvYGauge: string
-  // YEarn
+	// YEarn
 	ycrv3: string
 	ycrvTriCrypto2: string
-  ycrvUSDP: string
-  yDAI: string
+	ycrvUSDP: string
+	yDAI: string
 	yUSDC: string
 	ycrvSUSD: string
 	yWBTC: string
@@ -76,13 +76,13 @@ export class Tokens {
 	debtWETH: string
 
 	public constructor() {
-    // Basic Tokens
+		// Basic Tokens
 		this.weth = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 		this.wbtc = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
-    this.dai = '0x6b175474e89094c44da98b954eedeac495271d0f'
-    this.usdc = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-    this.usdt = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-    this.usdp = '0x1456688345527bE1f37E9e627DA0837D6f08C925'
+		this.dai = '0x6b175474e89094c44da98b954eedeac495271d0f'
+		this.usdc = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+		this.usdt = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+		this.usdp = '0x1456688345527bE1f37E9e627DA0837D6f08C925'
 		this.tusd = '0x0000000000085d4780B73119b644AE5ecd22b376'
 		this.usdn = '0x674C6Ad92Fd080e4004b2312b45f796a192D27a0'
 		this.eurs = '0xdB25f211AB05b1c97D595516F45794528a807ad8'
@@ -96,11 +96,11 @@ export class Tokens {
 		this.sUSD = '0x57ab1ec28d129707052df4df418d58a2d46d5f51'
 		this.sEUR = '0xd71ecff9342a5ced620049e616c5035f1db98620'
 		this.sETH = '0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb'
-    this.sBTC = '0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6'
+		this.sBTC = '0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6'
 		this.sAAVE = '0xd2df355c19471c8bd7d8a3aa27ff4e26a21b4076'
 		this.sLINK = '0xbBC455cb4F1B9e4bFC4B73970d360c8f032EfEE6'
-    this.sDOT = '0x1715ac0743102bf5cd58efbb6cf2dc2685d967b6'
-    this.sADA = '0xe36e2d3c7c34281fa3bc737950a68571736880a1'
+		this.sDOT = '0x1715ac0743102bf5cd58efbb6cf2dc2685d967b6'
+		this.sADA = '0xe36e2d3c7c34281fa3bc737950a68571736880a1'
 		// Aave Tokens
 		this.aWETH = '0x030bA81f1c18d280636F32af80b9AAd02Cf0854e'
 		this.aWBTC = '0x9ff58f4fFB29fA2266Ab25e75e2A8b3503311656'
@@ -108,7 +108,7 @@ export class Tokens {
 		this.aUSDC = '0xBcca60bB61934080951369a648Fb03DF4F96263C'
 		this.aUSDT = '0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811'
 		this.aCRV = '0x8dAE6Cb04688C62d939ed9B68d32Bc62e49970b1'
-    // Aave Debt Tokens
+		// Aave Debt Tokens
 		this.debtDAI = '0x778A13D3eeb110A4f7bb6529F99c000119a08E92'
 		this.debtUSDC = '0xE4922afAB0BbaDd8ab2a88E0C79d884Ad337fcA6'
 		this.debtWBTC = '0x51B039b9AFE64B78758f8Ef091211b5387eA717c'
@@ -117,11 +117,11 @@ export class Tokens {
 		this.cDAI = '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643'
 		this.cUSDC = '0x39aa39c021dfbae8fac545936693ac917d5e7563'
 		this.cUSDT = '0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9'
-    // Curve LP Tokens
-    this.crv3 = '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490'
+		// Curve LP Tokens
+		this.crv3 = '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490'
 		this.crvTriCrypto = '0xcA3d75aC011BF5aD07a98d02f18225F9bD9A6BDF'
 		this.crvTriCrypto2 = '0xc4ad29ba4b3c580e6d59105fff484999997675ff'
-    this.crvUSDP = '0x7Eb40E450b9655f4B3cC4259BCC731c63ff55ae6'
+		this.crvUSDP = '0x7Eb40E450b9655f4B3cC4259BCC731c63ff55ae6'
 		this.crvSUSD = '0xC25a3A3b969415c80451098fa907EC722572917F'
 		this.crvAAVE = '0xFd2a8fA60Abd58Efe3EeE34dd494cD491dC14900'
 		this.crvSAAVE = '0x02d341CcB60fAaf662bC0554d13778015d1b285C'
@@ -139,68 +139,68 @@ export class Tokens {
 		this.crvLINKGauge = '0xFD4D8a17df4C27c1dD245d153ccf4499e806C87D'
 		this.crvCOMPGauge = '0x7ca5b0a2910B33e9759DC7dDB0413949071D7575'
 		this.crvYGauge = '0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1'
-    // YEarn Tokens
+		// YEarn Tokens
 		this.ycrv3 = '0x84E13785B5a27879921D6F685f041421C7F482dA'
 		this.ycrvTriCrypto2 = '0xE537B5cc158EB71037D4125BDD7538421981E6AA'
-    this.ycrvUSDP = '0xC4dAf3b5e2A9e93861c3FBDd25f1e943B8D87417'
+		this.ycrvUSDP = '0xC4dAf3b5e2A9e93861c3FBDd25f1e943B8D87417'
 		this.ycrvSUSD = '0x5a770DbD3Ee6bAF2802D29a901Ef11501C44797A'
-    this.yDAI = '0xdA816459F1AB5631232FE5e97a05BBBb94970c95'
+		this.yDAI = '0xdA816459F1AB5631232FE5e97a05BBBb94970c95'
 		this.yUSDC = '0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE'
 		this.yWBTC = '0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E'
   }
 
   async registerTokens(owner: SignerWithAddress, strategyFactory: Contract, uniswapV3Registry?: Contract, chainlinkRegistry?: Contract, curveDepositZapRegistry?: Contract) {
-    await Promise.all([
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.RESERVE, ESTIMATOR_CATEGORY.DEFAULT_ORACLE, this.weth),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.RESERVE, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sUSD),
+		await Promise.all([
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.RESERVE, ESTIMATOR_CATEGORY.DEFAULT_ORACLE, this.weth),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.RESERVE, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sUSD),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.knc),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sEUR),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sLINK),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sEUR),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sLINK),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sETH),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sAAVE),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sBTC),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sDOT),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.SYNTH, ESTIMATOR_CATEGORY.CHAINLINK_ORACLE, this.sADA),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aWETH),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aWBTC),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aDAI),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aUSDC),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aUSDT),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aWETH),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aWBTC),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aDAI),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aUSDC),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aUSDT),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.AAVE_V2, this.aCRV),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtDAI),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtUSDC),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtWBTC),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtDAI),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtUSDC),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtWBTC),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.DEBT, ESTIMATOR_CATEGORY.AAVE_V2_DEBT, this.debtWETH),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.COMPOUND, this.cDAI),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.COMPOUND, this.cDAI),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.COMPOUND, this.cUSDC),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.COMPOUND, this.cUSDT),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crv3),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crv3),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvTriCrypto2),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvUSDP),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvSUSD),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvAAVE),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvUSDP),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvSUSD),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvAAVE),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvSAAVE),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvLINK),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvLINK),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvCOMP),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvUSDN),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvSETH),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvREN),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_LP, this.crvEURS),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvUSDPGauge),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvAAVEGauge),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvUSDPGauge),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvAAVEGauge),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvSAAVEGauge),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvCOMPGauge),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvLINKGauge),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrv3),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.CURVE_GAUGE, this.crvLINKGauge),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrv3),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrvTriCrypto2),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrvUSDP),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrvSUSD),
-      strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.yDAI),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrvUSDP),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.ycrvSUSD),
+			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.yDAI),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.yUSDC),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.YEARN_V2, this.yWBTC),
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.BLOCKED, this.crvTriCrypto), // Depreciated for TriCrypto2
 			strategyFactory.connect(owner).addItemToRegistry(ITEM_CATEGORY.BASIC, ESTIMATOR_CATEGORY.BLOCKED, '0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E') //TUSD second address
-    ])
+		])
 		if (uniswapV3Registry) {
 			await uniswapV3Registry.connect(owner).addPool(this.wbtc, this.weth, '3000') //0.3%
 			await uniswapV3Registry.connect(owner).addPool(this.usdc, this.weth, '3000') //0.3%
